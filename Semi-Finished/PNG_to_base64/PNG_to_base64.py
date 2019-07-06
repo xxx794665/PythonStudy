9a307384216c25e7640eee979a37b068efaf1f45
xxx794665 <249939260@qq.com>
2019-07-06 12:27:21 +0800

    fix PNG/Base64 convert

--- Semi-Finished/PNG_to_base64/PNG_to_base64.py
+++ Semi-Finished/PNG_to_base64/PNG_to_base64.py
@@ -1,7 +1,8 @@
 import base64
-f = open('F:\excuseme.png', 'rb')  #选择要转换的图片,并用二进制打开
+f = open(r'F:\flee.png', 'rb')  #选择要转换的图片,并用二进制打开
 ls_f = base64.b64encode(f.read())  #读取文件内容，并用base64编码
 f.close()
-file = open('base64.txt', 'wb')
+file = open(r'F:\flee.txt', 'wb')
 file.write(ls_f)
+file.flush()
 file.close()
