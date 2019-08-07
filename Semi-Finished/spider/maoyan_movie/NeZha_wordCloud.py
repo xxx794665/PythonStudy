afeca7ae7b53d6482cf8bdb8687a9d290e56391d
xxx794665 <249939260@qq.com>
2019-08-07 17:10:18 +0800

    semi-Finished pyeCharts+pandas

--- Semi-Finished/spider/maoyan_movie/NeZha_wordCloud.py
+++ Semi-Finished/spider/maoyan_movie/NeZha_wordCloud.py
@@ -8,10 +8,10 @@ from PIL import Image
 
 # 新定义分词词典
 jieba.load_userdict("dict.txt")
-df = pd.read_excel('猫眼_哪吒.xlsx')
+df = pd.read_csv('comments.txt', names=['id', 'nickName', 'cityName', 'content', 'score', 'startTime'])
 
 comments = str()
-for comment in df['评论内容']:
+for comment in df['content']:
     comments = comments + str(comment)
 
 stopwords = {}.fromkeys([line.rstrip() for line in open('chineseStopWords.txt')])
@@ -25,11 +25,11 @@ for seg in segs:
 fre = Counter(cloud_text)
 
 # 设置词云图背景
-mask = np.array(Image.open('background.jpg'))
+mask = np.array(Image.open('NeZha.png'))
 wc = wordcloud.WordCloud(
     font_path='PingFang Medium.ttf',  # 设置词云图字体格式
     mask=mask,  # 设置背景图
-    max_words=150,  # 最多显示词数
+    max_words=200,  # 最多显示词数
     max_font_size=150)  # 字体最大值
 
 wc.generate_from_frequencies(fre)  # 从字典生成词云
@@ -38,4 +38,4 @@ wc.recolor(color_func=image_colors)  # 将词云颜色设置为背景图方案
 plt.imshow(wc)  # 显示此云
 plt.axis('off')  # 关闭坐标轴
 plt.show()
-wc.to_file('CrazyAlien_comment_pic.png')
+wc.to_file('NeZha_comment_pic.png')
