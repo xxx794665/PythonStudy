f527f68fe9255f073e8366de88f2e8acbbb9968d
xxx794665 <249939260@qq.com>
2019-08-25 07:49:17 +0800

    format NeZha_spider

--- Semi-Finished/spider/maoyan_movie/NeZha_maoyan.py
+++ Semi-Finished/spider/maoyan_movie/NeZha_maoyan.py
@@ -12,7 +12,8 @@ from pymongo import MongoClient
 # 获取数据，根据url获取
 def get_data(url):
     headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.140 Safari/537.36'
+        'User-Agent':
+        'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.140 Safari/537.36'
     }
     req = request.Request(url, headers=headers)
     response = request.urlopen(req)
@@ -30,8 +31,9 @@ def parse_data(html):
             'id': item['id'],
             'nickName': item['nickName'],
             'userLevel': item['userLevel'],
-            'gender': item['gender'] if 'gender' in item else '',
-            'cityName': item['cityName'] if 'cityName' in item else '',  # 处理cityName不存在的情况
+            'gender': item['gender'] if 'gender' in item else '',  # 处理性别未设置的情况
+            'cityName':
+            item['cityName'] if 'cityName' in item else '',  # 处理城市名不存在的情况
             'content': item['content'].replace('\n', ' ', 10),  # 处理评论内容换行的情况
             'score': item['score'],
             'upCount': item['approve'],
@@ -48,8 +50,9 @@ def save_data():
     # start_time = '2019-08-21 09:24:58'
     # start_time = datetime.strptime(start_time, '%Y-%m-%d %H:%M:%S') + timedelta(seconds=-1)  # 转换为datetime类型，减1秒，避免获取到重复数据
     # start_time = datetime.strftime(start_time, '%Y-%m-%d %H:%M:%S')
-    start_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')  # 获取当前时间，从当前时间向前获取
-    end_time = '2019-07-26 00:00:00'  
+    # 获取当前时间，从当前时间向前获取
+    start_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    end_time = '2019-07-26 00:00:00'
     # 配置mongodb数据库
     host = os.environ.get('MONGODB_HOST', '127.0.0.1')  # 本地数据库
     port = os.environ.get('MONGODB_PORT', '27017')  # 数据库端口
@@ -58,7 +61,7 @@ def save_data():
     client = MongoClient(mongo_url)
     db = client[mongo_db]
     collection = db['Nezha_Comment']
-    collection.create_index('id', unique=True)  # 以评论的id为主键进行去重   
+    collection.create_index('id', unique=True)  # 以评论的id为主键进行去重
     while start_time > end_time:
         url = 'http://m.maoyan.com/mmdb/comments/movie/1211270.json?_v_=yes&offset=0&startTime=' + start_time.replace(
             ' ', '%20')
@@ -87,13 +90,14 @@ def save_data():
         # 将爬取的评论存入mongodb中
         for item in comments:
             collection.update_one({'id': item['id']}, {'$set': item},
-        upsert=True)
+                                  upsert=True)
         print(start_time)
         start_time = comments[14]['startTime']  # 获得末尾评论的时间
-        start_time = datetime.strptime(start_time, '%Y-%m-%d %H:%M:%S') + timedelta(
-            seconds=-1)  # 转换为datetime类型，减1秒，避免获取到重复数据
-        start_time = datetime.strftime(start_time, '%Y-%m-%d %H:%M:%S')  # 转换为str
-
+        start_time = datetime.strptime(
+            start_time, '%Y-%m-%d %H:%M:%S') + timedelta(
+                seconds=-1)  # 转换为datetime类型，减1秒，避免获取到重复数据
+        start_time = datetime.strftime(start_time,
+                                       '%Y-%m-%d %H:%M:%S')  # 转换为str
 
     print(start_time)
     print("爬取完成！")
